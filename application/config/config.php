59ea8a4671e3dd26784431421c48caf6bdfc0592
vetri-pixel <vetriselvam.pixel@gmail.com>
2021-10-18 17:37:47 +0530

    updated config file

--- application/config/config.php
+++ application/config/config.php
@@ -14,14 +14,14 @@ define('PAGEADMIN_URL','http://192.168.0.58/waycooldemo/');
 }
 else{
 
-$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].'/clients/waycool/beta/'; // Base URL including trailing slash (e.g. http://192.168.0.47/)
-$config['default_controller'] = 'main'; // Default controller to load
-$config['error_controller'] = 'error'; // Controller used for errors (e.g. 404, 500 etc)
-define('img_base_url','http://'.$_SERVER['SERVER_NAME'].'/clients/waycool/beta/uploads/'); // Base URL including trailing slash (e.g. http://192.168.0.47/)
-
-define('PAGEADMIN_URL','http://'.$_SERVER['SERVER_NAME'].'/clients/waycool/beta/');
-define('MAILLOGO','http://'.$_SERVER['SERVER_NAME'].'/clients/waycool/beta/uploads/logo/');
-define('resumeurl','http://'.$_SERVER['SERVER_NAME'].'/clients/waycool/beta/uploads/resume/');
+  $config['base_url'] = 'https://'.$_SERVER['SERVER_NAME'].'/'; // Base URL including trailing slash (e.g. http://192.168.0.47/)
+  $config['default_controller'] = 'main'; // Default controller to load
+  $config['error_controller'] = 'page_error'; // Controller used for errors (e.g. 404, 500 etc)
+  define('img_base_url','https://'.$_SERVER['SERVER_NAME'].'/'); // Base URL including trailing slash (e.g. http://192.168.0.47/)
+  
+  define('PAGEADMIN_URL','https://'.$_SERVER['SERVER_NAME'].'/');
+  define('MAILLOGO','https://'.$_SERVER['SERVER_NAME'].'/uploads/logo/');
+  define('resumeurl','https://'.$_SERVER['SERVER_NAME'].'/uploads/resume/');
 }
 
 ####for testing##########
@@ -47,7 +47,7 @@ if($_SERVER['HTTP_HOST'] == '192.168.0.47'){
   $config['db_password'] = 'mvc@123'; // Database password
 }
 
-define('DB_PREFIX',"akas_");
+define('DB_PREFIX',"way_");
 
 /**************************************** Database Tables *******************************/
 
@@ -59,26 +59,6 @@ define('tbl_carrier',DB_PREFIX."carrier_us");
 define('tbl_contact_us',DB_PREFIX."contact_us");
 define('tbl_clientlist',DB_PREFIX."clientlist");
 define('tbl_client',DB_PREFIX."client");
-
-define('tbl_projecttype',DB_PREFIX."projecttype");
-define('tbl_projectstatus',DB_PREFIX."projectstatus");
-define('tbl_project',DB_PREFIX."project");
-define('tbl_projectimg',DB_PREFIX."projectimg");
-define('tbl_projectenquiry',DB_PREFIX."projectenquiry");
-define('tbl_career',DB_PREFIX."career");
-define('tbl_location',DB_PREFIX."location");
-define('tbl_position',DB_PREFIX."position");
-define('tbl_announcement',DB_PREFIX."announcement");
-define('tbl_careers',DB_PREFIX."careers");
-define('tbl_popup_enquiry',DB_PREFIX."popup_enquiry");
-define('tbl_platinum_enquiry',DB_PREFIX."platinum_enquiry");
-
-define('tbl_nurse_training_enquiry',DB_PREFIX."nurse_training_enquiry");
-define('tbl_rnd_committee_enquiry',DB_PREFIX."rnd_committee_enquiry");
-
-define('listingcount','6');
-define('PRODUCTERROR','Content will be updated soon...');
-
 
 $config['storeid'] = '1';
 $config['openurl']=array('apply_now'); 
