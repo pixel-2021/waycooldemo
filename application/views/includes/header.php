aa8d41e0ca451e9c7005cc9905c6168b9ef90f67
pixel-2021 <91721359+pixel-2021@users.noreply.github.com>
2021-10-19 17:13:41 +0530

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -4,7 +4,7 @@
 	
 		<div class="container">
 		
-		 <a class="navbar-brand text-brand" href="./"><?xml version="1.0" encoding="utf-8"?>
+		 <a class="navbar-brand text-brand" href="./">
  
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 192 54" style="enable-background:new 0 0 192 54;" xml:space="preserve">
